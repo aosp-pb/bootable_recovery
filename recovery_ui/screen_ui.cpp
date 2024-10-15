LineageOS/android_bootable_recovery

recovery_ui: Respect margin_height while drawing battery capacity status
Some devices can use a custom margin height for
recovery. Consider this to draw battery capacity
status correctly.

Change-Id: Ib66e88a61db17d6b4735f7f706e718de26df83d8

recovery_ui/screen_ui.cpp
@@ -882,7 +882,7 @@ void ScreenRecoveryUI::draw_menu_and_text_buffer_locked(
 // Draws the battery capacity on the screen. Should only be called with updateMutex locked.
 void ScreenRecoveryUI::draw_battery_capacity_locked() {
   int x;
-  int y = gr_get_height(lineage_logo_.get());
+  int y = margin_height_ + gr_get_height(lineage_logo_.get());
   int icon_x, icon_y, icon_h, icon_w;
 
   // Battery status

aosp-pb/bootable_recovery

recovery_ui/screen_ui.cpp
@@ -867,7 +867,7 @@ void ScreenRecoveryUI::draw_menu_and_text_buffer_locked(
 // Draws the battery capacity on the screen. Should only be called with updateMutex locked.
 void ScreenRecoveryUI::draw_battery_capacity_locked() {
   int x;
-  int y = gr_get_height(lineage_logo_.get());
+  int y = margin_height_ + gr_get_height(lineage_logo.get());
   int icon_x, icon_y, icon_h, icon_w;
 
   // Battery status